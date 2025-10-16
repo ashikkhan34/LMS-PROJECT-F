38b515a3a9901cda845f970de996859b07bc3ecd
ashikkhan <ashikkhan314167@gmail.com>
2025-10-16 13:31:11 +0600

    update url

--- src/Hooks/useAxiosPublic.js
+++ src/Hooks/useAxiosPublic.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosPublic = axios.create({
-    baseURL:'https://lms-project-tau-inky.vercel.app/api/',
+    baseURL:'https://lms-project-3jv6m2s7m-ashik-khans-projects-431c6227.vercel.app/api',
     withCredentials: true,
 })
 
